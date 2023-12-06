b2756a5cc13bf54f31138e1ff9cf9fb8e1c8176b
eri-maulana <maulanabouble11@gmail.com>
2023-12-06 19:15:28 +0700

    fitst

---
+++ script.js
@@ -0,0 +1,30 @@
+let passwordLength = document.getElementById('passwordLength');
+let password = document.getElementById('password');
+let saveButton = document.getElementById('saveButton');
+
+
+function generatePassword(len){
+   const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
+   const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+   const numeric = 1234567890;
+   const symbol = "~`!@#$%^&*()_+-=|}{][';:?.,";
+
+   const data = lowerAlphabet + upperAlphabet + numeric + symbol;
+   let generator='';
+   for(let i = 0; i < len; i++){
+      generator += data[~~(Math.random() * data.length)];
+   }
+   return generator;
+}
+
+function getPassword(){
+   const newPassword = generatePassword(passwordLength.value);
+   password.value = newPassword;
+      alert('password has been generated');
+}
+
+const savePassword = () => {
+   document.title = password.value
+   saveButton.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(`password saya: ${document.title}`))
+   saveButton.setAttribute('download', 'MyPasswordGeneratorLOG.txt')
+ }
